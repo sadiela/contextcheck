64984cc9adf5ccf0b1d7d6dc95c07a9ed00e876b
Bayard Eton <beton@bu.edu>
2020-11-16 16:58:14 -0500

    Added textbox and mimic an API timeout on submit

--- react-flask-app/src/App.js
+++ react-flask-app/src/App.js
@@ -1,38 +1,49 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const formReducer = (state, event) => {
+    return {
+        ...state,
+        [event.target.name]: event.target.value
+    }
+}
+
 function App() {
+  const [formData, setFormData] = useReducer(formReducer, {});
+  const [submitting, setSubmitting] = useState(false);
   const [currentTime, setCurrentTime] = useState(0);
   useEffect(() => {
 		  fetch('/time').then(res => res.json()).then(data => {
 				  setCurrentTime(data.time);
 		  });
   }, []);
+  const handleSubmit = event => {
+    event.preventDefault();
+    setSubmitting(true);
+    setTimeout(() => {
+        setSubmitting(false);
+    }, 3000)
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <form>
-            <label> Text:
-                <input type="text" input="text"/>
-            </label>
-            <input type="submit" value="Submit"/>
-        </form>
+        <h1>Input Text Below</h1>
 		<p>The current time is {currentTime}.</p>
       </header>
+      {submitting &&
+        <div>Submitting...</div>
+      }
+      <form className="form" onSubmit={handleSubmit}>
+        <fieldset>
+            <label>
+                <p>Text Input</p>
+                <input defaultValue="Enter Text Here" onChange={setFormData}/>
+            </label>
+        </fieldset>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
