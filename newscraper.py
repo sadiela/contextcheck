963b8fb5d89a21e0bc13e930c93a40fc7140b45d
21ChenYe <ce.chenye@gmail.com>
2020-12-20 15:44:19 -0500

    cleaned up newscraper

--- newscraper.py
+++ newscraper.py
@@ -7,70 +7,93 @@ import time
 import config
 
 def article_parse(url):
+	if "cnn.com" in url:	
+		return cnnScrape(url)
+	elif "foxnews.com" in url:
+		return foxScrape(url)
+	elif "huffpost.com" in url:
+		return huffScrape(url)
+	elif "twitter.com" in url:
+		return tweetScrape(url)
+	else:
+		return "Invalid or unsupported URL"
+	
+def cnnScrape(url):
 	article = Article(url)	
 	try:
 		article.download()	
 	except:
 		print("Invalid URL or article.\nNote: Paywalled/subscriber articles will not work")
 		return "Error"
-		
-	if "cnn.com" in url:	
-		article.parse()
-		author = article.authors
-		tite = article.title
-		parseText = article.text.lower()
-		parseText = parseText.replace("\n", " ")
-		parseText = parseText.replace("\"", "")
-		feedText = parseText.split(".")
-		return title, author, feedText
+	article.parse()
+	author = article.authors
+	tite = article.title
+	date = article.publish_date
+	parseText = article.text.lower()
+	parseText = parseText.replace("\n", " ")
+	parseText = parseText.replace("\"", "")
+	feedText = parseText.split(".")
+	return title, author, feedText, date
 
-	elif "foxnews.com" in url:
-		article.parse()
-		author = article.authors
-		author = author[0]
-		title = article.title
-		parseText = article.text.lower()
-		parseText = parseText.replace("\n", " ")
-		parseText = parseText.replace("\"", "")
-		feedText = parseText.split(".")
-		for word in feedText:
-			if word.isupper():
-				feedText.remove(word)
-		return title, author, feedText
-	elif "huffpost.com" in url:
-		article.download()
-		article.parse()
-		huff_soup = requests.get(url, headers = {'User-Agent': 'Mozilla/5.0'})
-		huff_text = BeautifulSoup(huff_soup.text, 'html.parser')
-		author = huff_text.find("div", {"class":"author-list"}).text
-		title = article.title
-		response = requests.get(url, headers = {'User-Agent': 'Mozilla/5.0'})
-		if response.status_code != 200:
-			return "FAIL TO GET URL"
-		#article author does not work, utilize lxml
-		
-		parseText = article.text.lower()
-		parseText = parseText.replace("\n", " ")
-		parseText = parseText.replace("\"", "")
-		feedText = parseText.split(".")
-		return author, [feedText], title
-	elif "twitter.com" in url:
-		bearer_token = config.bearer
-		t_id = re.split("/status/", url)[1]
-		ids = "ids="+t_id
-		tweet_fields = "tweet.fields=lang,author_id"
-		auth = "expansions=author_id"
-		url = "https://api.twitter.com/2/tweets?{}&{}&{}".format(ids, tweet_fields, auth)
-		headers = {"Authorization": "Bearer {}".format(bearer_token)}
-		response = requests.request("GET", url, headers = headers)
-		twext = response.json()['data'][0]['text']
-		atext = response.json()['includes']['users'][0]['username']
-		return twext, atext
+def foxScrape(url):
+	article = Article(url)	
+	try:
+		article.download()	
+	except:
+		print("Invalid URL or article.\nNote: Paywalled/subscriber articles will not work")
+		return "Error"
+	article.parse()
+	author = article.authors
+	author = author[0]
+	date = 0
+	title = article.title
+	parseText = article.text.lower()
+	parseText = parseText.replace("\n", " ")
+	parseText = parseText.replace("\"", "")
+	feedText = parseText.split(".")
+	for word in feedText:
+		if word.isupper():
+			feedText.remove(word)
+	return title, author, feedText, date
+
+def huffScrape(url):
+	article = Article(url)	
+	try:
+		article.download()	
+	except:
+		print("Invalid URL or article.\nNote: Paywalled/subscriber articles will not work")
+		return "Error"
+	article.parse()
+	huff_soup = requests.get(url, headers = {'User-Agent': 'Mozilla/5.0'})
+	huff_text = BeautifulSoup(huff_soup.text, 'html.parser')
+	author = huff_text.find("div", {"class":"author-list"}).text
+	title = article.title
+	date = article.publish_date
+	response = requests.get(url, headers = {'User-Agent': 'Mozilla/5.0'})
+	if response.status_code != 200:
+		return "FAIL TO GET URL"
+	#article author does not work, utilize lxml
 	
+	parseText = article.text.lower()
+	parseText = parseText.replace("\n", " ")
+	parseText = parseText.replace("\"", "")
+	feedText = parseText.split(".")
+	return author, [feedText], title, date
+
+def tweetScrape(url):
+	bearer_token = config.bearer
+	t_id = re.split("/status/", url)[1]
+	ids = "ids="+t_id
+	tweet_fields = "tweet.fields=lang,author_id"
+	auth = "expansions=author_id"
+	url = "https://api.twitter.com/2/tweets?{}&{}&{}".format(ids, tweet_fields, auth)
+	headers = {"Authorization": "Bearer {}".format(bearer_token)}
+	response = requests.request("GET", url, headers = headers)
+	twext = response.json()['data'][0]['text']
+	atext = response.json()['includes']['users'][0]['username']
+	return twext, atext
 	
-#to do: 
-# date time functionality
-# subscription based...?
+
 def main():
 	tic = time.perf_counter()
 	url = 'https://www.huffpost.com/entry/covid-19-eviction-crisis-women_n_5fca8af3c5b626e08a29de11'
