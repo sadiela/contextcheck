e3784413999b14caff9732a9dd30bb9d434f10c7
Ye Chen <ce.chenye@gmail.com>
2021-02-06 03:04:38 -0800

    Add files via upload
    added meta data parsing to fox news for better keyword input into NewsAPI

--- newscraper.py
+++ newscraper.py
@@ -4,7 +4,7 @@ import requests, re, json
 from bs4 import BeautifulSoup
 #import eval
 import time
-import config
+#import config
 
 def article_parse(url):
 	if "cnn.com" in url:	
@@ -54,7 +54,11 @@ def foxScrape(url): #run time ~.2 seconds
 	for word in feedText:
 		if word.isupper():
 			feedText.remove(word)
-	return title, author, feedText, date
+	re = requests.get(url, headers = {'User-Agent':'Mozilla/5.0'})
+	fox_soup = BeautifulSoup(re.text, 'html.parser')
+	meta = fox_soup.find("meta", {"name":"classification-isa"})['content']
+	meta = meta.split(',')
+	return title, author, feedText, date, meta
 
 def huffScrape(url): #runtime ~1.2-1.4 seconds
 	article = Article(url)	
@@ -79,7 +83,7 @@ def huffScrape(url): #runtime ~1.2-1.4 seconds
 	parseText = parseText.replace("\"", "")
 	feedText = parseText.split(".")
 	return title, author, [feedText], date
-
+'''
 def tweetScrape(url): #runtime sub .4 seconds
 	bearer_token = config.bearer
 	t_id = re.split("/status/", url)[1]
@@ -93,7 +97,7 @@ def tweetScrape(url): #runtime sub .4 seconds
 	atext = response.json()['includes']['users'][0]['username']
 	return twext, atext
 	
-
+'''
 def main():
 	tic = time.perf_counter()
 	url = 'https://www.huffpost.com/entry/covid-19-eviction-crisis-women_n_5fca8af3c5b626e08a29de11'
@@ -101,11 +105,13 @@ def main():
 	url3 = 'https://www.foxnews.com/politics/ilhan-omar-slams-lawmakers-vaccine'
 	url4 = 'https://www.cnn.com/2020/12/21/politics/bidens-coronavirus-vaccination/index.html'
 	#author, feedText, title =  article_parse(url)
-	author, title, text, date = article_parse(url4)
+	author, title, text, date, meta = article_parse(url3)
 	print(author)
 	print(text)
 	print(title)
 	print(date)
+	print(meta)
+	
 	toc = time.perf_counter()
 	print(f"\nRuntime = {toc - tic:0.4f} seconds") 
 
